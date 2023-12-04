f9e093cc1162c5e41797fae63beca3c868f3bde9
fcecin <fcecin@gmail.com>
2023-12-04 18:14:17 -0300

    clean up expr.hpp

--- expr.hpp
+++ expr.hpp
@@ -34,13 +34,6 @@
   This is basically a hack of the code it is based on to produce the GASM
   program string. There's a bunch of dead code and meaningless values generated
   because we are not interested in actually evaluating the expression.
-
-  -----------------------------------------------------------------------------
-
-  TODO:
-
-  - clean up
-
 */
 
 #include <iostream>
@@ -51,8 +44,6 @@
 #include <tuple>
 #include <stdio.h>
 #include <math.h>
-
-static const char* reportFmt = "|%-5s|%-32s|%17s| %s\n";
 
 class Token {
 public:
@@ -79,50 +70,18 @@ public:
       {}
 
    const Type type;
-   std::string str;
+   const std::string str;
    const int precedence;
    const bool rightAssociative;
    const bool unary;
 };
 
+
 std::ostream& operator<<(std::ostream& os, const Token& token) {
    if (token.type == Token::Type::Register)
       os << "@";
-
    os << token.str;
    return os;
-}
-
-
-// Debug output
-template<class T, class U>
-void debugReport(
-   const Token& token
-   , const T& queue
-   , const U& stack
-   , const std::string& comment = ""
-   ) {
-   std::ostringstream ossQueue;
-   for(const auto& t : queue) {
-      ossQueue << " " << t;
-   }
-
-   std::ostringstream ossStack;
-   for(const auto& t : stack) {
-      ossStack << " " << t;
-   }
-
-   std::ostringstream ossToken;
-   ossToken << token;
-
-#ifdef DEBUG
-   printf(reportFmt
-          , ossToken.str().c_str() //token.str.c_str()
-          , ossQueue.str().c_str()
-          , ossStack.str().c_str()
-          , comment.c_str()
-      );
-#endif
 }
 
 
@@ -156,17 +115,13 @@ std::deque<Token> exprToTokens(const std::string& expr) {
          bool unary = false;
          char c = *p;
          char c2 = *(p + 1);
-
          int opsz = 1;
 
          switch(c) {
          default:                                    break;
          case '(':   t = Token::Type::LeftParen;     break;
          case ')':   t = Token::Type::RightParen;    break;
-
-            //XOR
-         case '^':   t = Token::Type::Operator;      precedence = 4; /*rightAssociative = true;*/  break;
-
+         case '^':   t = Token::Type::Operator;      precedence = 4; break;
          case '*':   t = Token::Type::Operator;      precedence = 10; break;
          case '/':   t = Token::Type::Operator;      precedence = 10; break;
          case '%':   t = Token::Type::Operator;      precedence = 10; break;
@@ -178,8 +133,6 @@ std::deque<Token> exprToTokens(const std::string& expr) {
                   || tokens.back().type == Token::Type::Operator
                   || tokens.back().type == Token::Type::LeftParen
                ) {
-               //std::cout << "ERROR: - is not a unary operator" << std::endl;
-               //exit(1);
                throw std::runtime_error("ERROR: - is not a unary operator");
             } else {
                // otherwise, it's binary '-'
@@ -195,15 +148,10 @@ std::deque<Token> exprToTokens(const std::string& expr) {
                   || tokens.back().type == Token::Type::LeftParen
                ) {
                // it's unary '~'
-               // note#1 : 'n' is a special operator name for unary '~'
-               // note#2 : It has highest precedence than any of the infix operators
                unary = true;
-               //c = '~';
                t = Token::Type::Operator;
                precedence = 11;
             } else {
-               //std::cout << "ERROR: ~ is not a binary operator" << std::endl;
-               //exit(1);
                throw std::runtime_error("ERROR: ~ is not a binary operator");
             }
             break;
@@ -222,7 +170,7 @@ std::deque<Token> exprToTokens(const std::string& expr) {
             switch (c2) {
             case '|':   // ||
                opsz = 2;
-               //break;   || and | behave identically
+               //break;   || and | generate the same opcode ('OR')
             default:    // just |
                t = Token::Type::Operator;      precedence = 3; break;
                break;
@@ -282,10 +230,7 @@ std::deque<Token> exprToTokens(const std::string& expr) {
                   )
                {
                   // it's unary '!'
-                  // note#1 : 'n' is a special operator name for unary '!'
-                  // note#2 : It has highest precedence than any of the infix operators
                   unary = true;
-                  //c = '!';
                   t = Token::Type::Operator;
                   precedence = 11;
                } else {
@@ -321,7 +266,6 @@ std::deque<Token> shuntingYard(const std::deque<Token>& tokens) {
       // Read a token
       switch(token.type) {
       case Token::Type::Register:
-         //std::cout << "pushed register onto stack: " << token << std::endl;
       case Token::Type::Number:
          // If the token is a number, then add it to the output queue
          queue.push_back(token);
@@ -349,14 +293,11 @@ std::deque<Token> shuntingYard(const std::deque<Token>& tokens) {
                stack.pop_back();
                // onto the output queue;
                queue.push_back(o2);
-
                continue;
             }
-
-            // @@ otherwise, exit.
+            // otherwise, exit.
             break;
          }
-
          // push o1 onto the stack.
          stack.push_back(o1);
       }
@@ -385,10 +326,7 @@ std::deque<Token> shuntingYard(const std::deque<Token>& tokens) {
          if(!match && stack.empty()) {
             // If the stack runs out without finding a left parenthesis,
             // then there are mismatched parentheses.
-            //printf("RightParen error (%s)\n", token.str.c_str());
-
             throw std::runtime_error("ERROR: RightParen error");
-
             return {};
          }
 
@@ -399,12 +337,9 @@ std::deque<Token> shuntingYard(const std::deque<Token>& tokens) {
       break;
 
       default:
-         //printf("error (%s)\n", token.str.c_str());
          throw std::runtime_error("ERROR: (token): " + token.str);
          return {};
       }
-
-      debugReport(token, queue, stack);
    }
 
    // When there are no more tokens to read:
@@ -413,9 +348,7 @@ std::deque<Token> shuntingYard(const std::deque<Token>& tokens) {
       // If the operator token on the top of the stack is a parenthesis,
       // then there are mismatched parentheses.
       if(stack.back().type == Token::Type::LeftParen) {
-         //printf("Mismatched parentheses error\n");
          throw std::runtime_error("ERROR: Mismatched parentheses error");
-
          return {};
       }
 
@@ -424,269 +357,192 @@ std::deque<Token> shuntingYard(const std::deque<Token>& tokens) {
       stack.pop_back();
    }
 
-   debugReport(Token { Token::Type::Unknown, "End" }, queue, stack);
-
    //Exit.
    return queue;
 }
 
-// instead of evaluating a result, this prints out the assembly propgram
+
+// instead of evaluating a result, this returns the assembly program
 std::string expressionToGASM(const std::string& expr, bool lf) {
 
    // gasm output stream
    std::ostringstream ossg;
 
+   // we don't care about actually computing the expression, so
+   //  this replaces the result of each operation.
    std::string zero = "0";
 
    char sep;
-   if (lf) {
+   if (lf)
       sep = '\n';
-   } else {
+   else
       sep = ' ';
-   }
 
-#ifdef DEBUG
-   printf("Tokenize\n");
-   printf(reportFmt, "Token", "Queue", "Stack", "");
-#endif
-   
    const auto tokens = exprToTokens(expr);
    auto queue = shuntingYard(tokens);
    std::vector<Token> stack;
 
-#ifdef DEBUG
-   printf("\nCalculation\n");
-   printf(reportFmt, "Token", "Queue", "Stack", "");
-#endif
-   
    while(! queue.empty()) {
-      std::string op;
-
       const auto token = queue.front();
       queue.pop_front();
       switch(token.type) {
       case Token::Type::Register:
-         stack.push_back(token);
-         op = "Push @" + token.str;
-#ifdef DEBUG
-         std::cout << "ASM OPERATIO1: PUSH " << token << std::endl;
-#endif
-         ossg << "PUSH " << token << sep;
-         break;
-
       case Token::Type::Number:
          stack.push_back(token);
-         op = "Push " + token.str;
-#ifdef DEBUG
-         std::cout << "ASM OPERATIO2: PUSH " << token << std::endl;
-#endif
          ossg << "PUSH " << token << sep;
          break;
 
       case Token::Type::Operator:
       {
          if(token.unary) {
-            // unray operators
-            Token rhs = stack.back();
-            stack.pop_back();
+            stack.pop_back(); // rhs
             switch(token.str[0]) {
             default:
-               //printf("Operator error [%s]\n", token.str.c_str());
-               //exit(0);
-               throw std::runtime_error("ERROR: Operator error");
+               throw std::runtime_error("ERROR: Operator error: " + token.str);
                break;
-               
-               //    VM literally doesn't support an unary -  since everything is uint64_t
-
             case '~':                   // Special operator name for unary '~'
-               rhs.str = std::to_string( ~ std::stoi(rhs.str) );
-               stack.push_back( rhs );
-
+               stack.push_back(Token { Token::Type::Number, zero });
                ossg << "NEG" << sep;
                break;
             case '!':                   // Special operator name for unary '~'
-               rhs.str = std::to_string( ! std::stoi(rhs.str) );
-               stack.push_back( rhs );
-
+               stack.push_back(Token { Token::Type::Number, zero });
                ossg << "NOT" << sep;
                break;
-               
             }
-            //op = "Push (unary) " + token.str + " " + std::to_string(rhs);
-#ifdef DEBUG
-            op = "Push (unary) something";
-#endif
          } else {
             // binary operators
-            auto rhs = stack.back();
-            stack.pop_back();
-            auto lhs = stack.back();
-            stack.pop_back();
+            stack.pop_back(); // rhs
+            stack.pop_back(); // lhs
 
             char c2 = ' ';
-            if (token.str.size() > 1) {
+            if (token.str.size() > 1)
                c2 = token.str[1];
-            }
 
             switch(token.str[0]) {
             default:
-               ossg << "UNKNOWN_OPERATION" << sep;
-               throw std::runtime_error("ERROR: Operator error (2)");
-               //printf("Operator error [%s]\n", token.str.c_str());
-               //exit(0);
-               throw std::runtime_error("ERROR: Operator error (2)");
-               
+               throw std::runtime_error("ERROR: Operator error (2): " + token.str);
                break;
             case '^':
                ossg << "XOR" << sep;
-               //stack.push_back(static_cast<int>(pow(lhs, rhs)));
                stack.push_back(Token { Token::Type::Number, zero });
                break;
             case '*':
                ossg << "MUL" << sep;
-               //stack.push_back(lhs * rhs);
                stack.push_back(Token { Token::Type::Number, zero });
                break;
             case '/':
                ossg << "DIV" << sep;
-               //stack.push_back(lhs / rhs);
                stack.push_back(Token { Token::Type::Number, zero });
                break;
             case '+':
                ossg << "ADD" << sep;
-               //stack.push_back(lhs + rhs);
                stack.push_back(Token { Token::Type::Number, zero });
                break;
             case '-':
                ossg << "SUB" << sep;
-               //stack.push_back(lhs - rhs);
                stack.push_back(Token { Token::Type::Number, zero });
                break;
-
             case '%':
                ossg << "MOD" << sep;
-               //stack.push_back(lhs / rhs);
                stack.push_back(Token { Token::Type::Number, zero });
                break;
-
-
-         case '&':
-            switch (c2) {
-            case '&':   // &&
-               ossg << "BAND" << sep;
-               stack.push_back(Token { Token::Type::Number, zero });
+            case '&':
+               switch (c2) {
+               case '&':   // &&
+                  ossg << "BAND" << sep;
+                  stack.push_back(Token { Token::Type::Number, zero });
+                  break;
+               case ' ':    // just &
+                  ossg << "AND" << sep;
+                  stack.push_back(Token { Token::Type::Number, zero });
+                  break;
+               default:
+                  throw std::runtime_error("ERROR: Operator error (8): " + token.str);
+                  break;
+               }
                break;
-            case ' ':    // just &
-               ossg << "AND" << sep;
-               stack.push_back(Token { Token::Type::Number, zero });
+            case '|':
+               switch (c2) {
+               case '|':   // ||
+                  //break;   || and | behave identically
+               case ' ':    // just |
+                  ossg << "OR" << sep;
+                  stack.push_back(Token { Token::Type::Number, zero });
+                  break;
+               default:
+                  throw std::runtime_error("ERROR: Operator error (7): " + token.str);
+                  break;
+               }
                break;
-            default:
-               ossg << "UNKNOWN_OPERATION" << sep;
-               throw std::runtime_error("ERROR: Operator error (8)");
+            case '<':
+               switch (c2) {
+               case '<':   // <<
+                  ossg << "SHL" << sep;
+                  stack.push_back(Token { Token::Type::Number, zero });
+                  break;
+               case '=':   // <=
+                  ossg << "LE" << sep;
+                  stack.push_back(Token { Token::Type::Number, zero });
+                  break;
+               case ' ':    // <
+                  ossg << "LT" << sep;
+                  stack.push_back(Token { Token::Type::Number, zero });
+                  break;
+               default:
+                  throw std::runtime_error("ERROR: Operator error (6): " + token.str);
+                  break;
+               }
+               break;
+            case '>':
+               switch (c2) {
+               case '>':   // >>
+                  ossg << "SHR" << sep;
+                  stack.push_back(Token { Token::Type::Number, zero });
+                  break;
+               case '=':   // >=
+                  ossg << "GE" << sep;
+                  stack.push_back(Token { Token::Type::Number, zero });
+                  break;
+               case ' ':    // >
+                  ossg << "GT" << sep;
+                  stack.push_back(Token { Token::Type::Number, zero });
+                  break;
+               default:
+                  throw std::runtime_error("ERROR: Operator error (5): " + token.str);
+                  break;
+               }
+               break;
+            case '=':
+               switch (c2) {
+               case '=':   // ==
+                  ossg << "EQ" << sep;
+                  stack.push_back(Token { Token::Type::Number, zero });
+                  break;
+               default:    // '='  (not supported) -- doing the Unknown token case
+                  throw std::runtime_error("ERROR: Operator error (3): " + token.str);
+                  break;
+               }
+               break;
+            case '!':
+               switch (c2) {
+               case '=':   // !=
+                  ossg << "NE" << sep;
+                  stack.push_back(Token { Token::Type::Number, zero });
+                  break;
+               default:
+                  throw std::runtime_error("ERROR: Operator error (4): " + token.str);
+                  break;
+               }
                break;
             }
-            break;
-         case '|':
-            switch (c2) {
-            case '|':   // ||
-               //break;   || and | behave identically
-            case ' ':    // just |
-               ossg << "OR" << sep;
-               stack.push_back(Token { Token::Type::Number, zero });
-               break;
-            default:
-               ossg << "UNKNOWN_OPERATION" << sep;
-               throw std::runtime_error("ERROR: Operator error (7)");
-               break;
-            }
-            break;
-         case '<':
-            switch (c2) {
-            case '<':   // <<
-               ossg << "SHL" << sep;
-               stack.push_back(Token { Token::Type::Number, zero });
-               break;
-            case '=':   // <=
-               ossg << "LE" << sep;
-               stack.push_back(Token { Token::Type::Number, zero });
-               break;
-            case ' ':    // <
-               ossg << "LT" << sep;
-               stack.push_back(Token { Token::Type::Number, zero });
-               break;
-            default:
-               ossg << "UNKNOWN_OPERATION" << sep;
-               throw std::runtime_error("ERROR: Operator error (6)");
-               break;
-            }
-            break;
-         case '>':
-            switch (c2) {
-            case '>':   // >>
-               ossg << "SHR" << sep;
-               stack.push_back(Token { Token::Type::Number, zero });
-               break;
-            case '=':   // >=
-               ossg << "GE" << sep;
-               stack.push_back(Token { Token::Type::Number, zero });
-               break;
-            case ' ':    // >
-               ossg << "GT" << sep;
-               stack.push_back(Token { Token::Type::Number, zero });
-               break;
-            default:
-               ossg << "UNKNOWN_OPERATION" << sep;
-               throw std::runtime_error("ERROR: Operator error (5)");
-               break;
-            }
-            break;
-         case '=':
-            switch (c2) {
-            case '=':   // ==
-               ossg << "EQ" << sep;
-               stack.push_back(Token { Token::Type::Number, zero });
-               break;
-            default:    // '='  (not supported) -- doing the Unknown token case
-               ossg << "UNKNOWN_OPERATION" << sep;
-               throw std::runtime_error("ERROR: Operator error (3)");
-               break;
-            }
-            break;
-         case '!':
-            switch (c2) {
-            case '=':   // !=
-               ossg << "NE" << sep;
-               stack.push_back(Token { Token::Type::Number, zero });
-               break;
-            default:
-               ossg << "UNKNOWN_OPERATION" << sep;
-               throw std::runtime_error("ERROR: Operator error (4)");
-               break;
-            }
-            break;
-
-
-               
-               
-            }
-            //op = "Push " + std::to_string(lhs) + " " + token.str + " " + std::to_string(rhs);
-            op = "Push something";
-#ifdef DEBUG            
-            std::cout << "ASM OPERATION: " << token.str << "    (garbage: " << lhs << " " << rhs << std::endl;
-#endif
          }
       }
       break;
 
       default:
-         //printf("Token error\n");
-         //exit(0);
-         throw std::runtime_error("ERROR: Token error");
+         throw std::runtime_error("ERROR: Token error: " + token.str);
       }
-      debugReport(token, queue, stack, op);
    }
 
    return ossg.str();
-
-   //return stack.back();
 }
